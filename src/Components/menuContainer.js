7db1cfd0c6e19671a5ecc30dc54b78df93d580b4
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-21 16:16:19 +0100

    feat(menu): add ability to upload menu and post to server

--- src/Components/menuContainer.js
+++ src/Components/menuContainer.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React from 'react';
 import csv from 'csvtojson';
 
@@ -14,25 +13,30 @@ const convertToObject = (input) => {
 // temporary stuff using Egill's token
 const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5qY3pOVEV6T1RKRE4wTXlRMEZDTURJeE56TTVRMEU1UWpaRk5qWTROVE5FUXpSQ09EUXhNQSJ9.eyJnaXZlbl9uYW1lIjoiRWdpbGwiLCJmYW1pbHlfbmFtZSI6IkhyZWluc3NvbiIsIm5pY2tuYW1lIjoiZWdpbGxoMjEwIiwibmFtZSI6IkVnaWxsIEhyZWluc3NvbiIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLTVZVmVodUQ1QXRRL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFjL1R6UG1fV3duSElJL3Bob3RvLmpwZyIsImxvY2FsZSI6ImlzIiwidXBkYXRlZF9hdCI6IjIwMTktMDMtMTlUMTc6NTA6NTkuMjk2WiIsImVtYWlsIjoiZWdpbGxoMjEwQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mNG5jNzI0ZC5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDcwNjk4Mjc2ODMzNzc5MzY0MzQiLCJhdWQiOiJQUWhzdXlTd3c3cWNBbnlMUmFWbGQxTzVmWXh6UjhCYiIsImlhdCI6MTU1MzAxNzg2MCwiZXhwIjoxNTUzMDUzODYwLCJhdF9oYXNoIjoia3RFekhacE9hRGd4NFNScVJEYmdfdyIsIm5vbmNlIjoiNzZYcm9Hdlh4dFU2Uzl6M2NmOHBUWDdJTzAwbDgyeUQifQ.4QuX5elDwvi3N23NhzONjqZ6f2rMkVsVllEsko7q5wh2CtLeAUwY8dCjxgNXXFAPFWJKoTrpcRO2U9lQFoKDvxT6aOr2NQbw4nUGoPDFTrZ7u3TstEDc-ARdYQd05-cXtisuRxeFOfOSz30-Oft5-5AOxrfoU9ZNN5wJc2UkCPDZbFyR2McDYQR6I5C8fp9FZI3AXuTsDDAS4zZPIY63fz1y-uTRoVQSHVVPVLQT88Uymns9UBj79pMZJYuS-hdOH4-wkpUdjCaHbRBkSaw495YFcKXILjc8l9sM2oKc3ngb6OgLyNPaguyiFfHM-gsU460jMszgse7lZJPwEflRiA';
 const BAR_ID = '7qvPtpnc7';
-const SERVER_PORT = 3001;
-const SERVER_ADDRESS = `http://localhost:${SERVER_PORT}/owner/bars/${BAR_ID}/menus`;
+const SERVER_ADDRESS = `/owner/bars/${BAR_ID}/menus`;
 
 class MenuContainer extends React.Component {
   state = {
     file: {},
     json: [],
+    menuName: '',
   }
 
-  handleChange = (files) => {
+  onChangeName = (e) => {
+    this.setState({
+      menuName: e.nativeEvent.target.value,
+    });
+  }
+
+  onFileChange = (e) => {
+    const { files } = e.target;
     this.setState({ file: files[0] });
   }
 
-  onSubmit = (event) => {
-    event.preventDefault();
+  onSubmit = (e) => {
+    e.preventDefault();
     const { file } = this.state;
     const reader = new FileReader();
-    reader.onabort = () => console.log('file reading was aborted');
-    reader.onerror = () => console.log('file reading has failed');
     reader.onload = () => {
       const binaryStr = reader.result;
       csv({ noheader: false, output: 'csv' })
@@ -49,8 +53,10 @@ class MenuContainer extends React.Component {
     reader.readAsBinaryString(file);
   }
 
-  onConfirm = async () => {
-    const { json } = this.state;
+  onConfirm = async (e) => {
+    e.preventDefault();
+    const { json, menuName } = this.state;
+    const newMenu = { name: menuName, categories: json };
     const result = await fetch(
       SERVER_ADDRESS, {
         method: 'POST',
@@ -58,17 +64,13 @@ class MenuContainer extends React.Component {
           Authorization: `Bearer ${TOKEN}`,
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(json),
+        body: JSON.stringify(newMenu),
       },
     );
     console.log(result);
   }
 
-  onChangeName = () => {
-    console.log('added a name');
-  }
-
-  computeMenu = (jsonMenu) => {
+  renderMenu = (jsonMenu) => {
     const renderCategory = category => (
       <div key={category.name}>
         <div className="category__title">{category.name}</div>
@@ -90,12 +92,12 @@ class MenuContainer extends React.Component {
       <div className="menuContainer">
         <form>
           <input type="text" placeholder="Name" onChange={this.onChangeName} />
-          <input type="file" name="file" onChange={e => this.handleChange(e.target.files)} />
+          <input type="file" name="file" onChange={this.onFileChange} />
           <input type="submit" value="Submit" onClick={this.onSubmit} />
-          <input type="submit" value="Confirm" onClick={this.onConfirm} />
+          <input type="submit" value="Submit" onClick={this.onConfirm} />
         </form>
         <div>
-          {this.computeMenu(json)}
+          {this.renderMenu(json)}
         </div>
       </div>
     );
